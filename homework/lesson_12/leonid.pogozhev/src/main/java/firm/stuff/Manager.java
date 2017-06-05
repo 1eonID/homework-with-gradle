fca026c043c9e09b5e992d6bbc91ea0287b4493f
1eonID <lpogojev@gmail.com>
2017-06-05 11:43:58 +0300

    Add homework from lesson 12 by Leonid Pogozhev

---
+++ homework/lesson_12/leonid.pogozhev/src/main/java/firm/stuff/Manager.java
@@ -0,0 +1,44 @@
+package firm.stuff;
+
+import static firm.PaymentPeriod.BIWEEKLY;
+import static firm.PaymentPeriod.MONTHLY;
+import static firm.PaymentPeriod.WEEKLY;
+
+import firm.Employee;
+import firm.PaymentPeriod;
+
+public class Manager extends Employee {
+  private String name;
+  private float dayRate;
+  private float workDays;
+  private PaymentPeriod period;
+  private float bonus;
+
+  public Manager(String name, float dayRate, PaymentPeriod period) {
+    this.name = name;
+    this.dayRate = dayRate;
+    this.period = period;
+  }
+
+  public float calculatePayment() {
+    return (workDays * dayRate) + bonus;
+  }
+
+  public void setWorkingHours(int workingHours) {
+    float koefWeekly = (float)Math.floor(workingHours / (5.00f * WORKING_HOURS_PER_DAY));
+    float koefBiweekly = (float)Math.floor(workingHours / (10.00f * WORKING_HOURS_PER_DAY));
+    float koefMonthly = (float)Math.floor(workingHours / (20.00f * WORKING_HOURS_PER_DAY));
+
+    if (period == WEEKLY) {
+      workDays = koefWeekly * 5.00f;
+    } else if (period == BIWEEKLY) {
+      workDays = koefBiweekly * 10.00f;
+    } else if (period == MONTHLY) {
+      workDays = koefMonthly * 20.00f;
+    }
+  }
+
+  public void setBonus(float inBonus) {
+    bonus = inBonus;
+  }
+}
