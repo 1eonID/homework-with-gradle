9fceea0bfc85e3861e4cb740d318c5ce0599f010
Leonid Pogozhev <lpogojev@gmail.com>
2017-07-17 07:54:14 +0300

    Add files for HW from lesson 22, only empty files, without imlementation

---
+++ homework/lesson_22/leonid.pogozhev/src/test/java/MyBSTMapTests.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Ignore;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import algo.PriorityQueue;
+import algo.PriorityQueueSorter;
+import java.util.Iterator;
+
+import java.util.Arrays;
+
+public class MyBSTMapTests {
+  private BSTMap<Integer, String> map;
+
+  @Before
+  public void setUp() {
+    map = new BSTMap<>();
+  }
+
+  @Test
+  public void testSimpleElemAddition() {
+
+  }
+
+  @Test
+  public void testFewElemsAddition() {
+
+  }
+
+  @Test
+  public void testOnZeroSize() {
+
+  }
+
+  @Test
+  public void testManyAdditions() {
+
+  }
+
+}
